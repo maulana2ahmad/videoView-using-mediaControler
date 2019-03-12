9cc6265353070ec4b30d3016259936a9203980e9
Captain America <_REDUDANT@MNCMAC.local>
2019-03-12 14:32:37 +0700

    videoView using mediaControler

---
+++ app/src/main/java/com/redudant/videodemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.redudant.videodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.a);
+
+        MediaController mediaController = new MediaController(this);
+
+        //setting mediaControler in videoView
+        mediaController.setAnchorView(videoView);
+
+        //setting videoView using mediaControler
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+    }
+}
